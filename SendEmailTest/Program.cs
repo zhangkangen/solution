01a6f755ff2376e416372b13a5515f16683fd0ab
zhang <zhangkangen@gmail.com>
2017-02-10 10:46:00 +0800

    添加项目文件。

---
+++ SendEmailTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmailTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailAddress fromAddress = new MailAddress("robotmsg@126.com");
+            MailAddress toAddress = new MailAddress("kangeneral@126.com");
+
+            MailMessage mail = new MailMessage(fromAddress, toAddress);
+            mail.Subject = "新的书籍";
+            mail.Body = "查看这些新书";
+            mail.IsBodyHtml = true;
+            mail.BodyEncoding = Encoding.Default;
+            mail.Priority = MailPriority.Normal;
+
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.126.com";
+            client.Port = 25;
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("robotmsg@126.com", "q1w2e3r4");
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Send(mail);
+        }
+    }
+}
---
+++ SqliteTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //SQLiteConnection conn = new SQLiteConnection("data Source=test.sqlite;password=123456");
+            //conn.Open();
+            //conn.ChangePassword("123456");
+
+            ISessionFactory sessionFactory = Fluently.Configure()
+                .BuildConfiguration()
+                .BuildSessionFactory();
+
+            ISession session = sessionFactory.OpenSession();
+
+            int returnCount = session.CreateSQLQuery("create table s_user(id text,name text);insert into s_user values('1','1')")
+                .ExecuteUpdate();
+
+        }
+    }
+}
